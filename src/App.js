40c5f5535e805a7083a32f5827bbdba3f16b9c51
hanangaffari <gaffarihanan@yahoo.com>
2022-07-25 20:50:21 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import {FiSettings} from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 
 const App = () => {
+  const activeMenu = false;
   return (
     <div>
       <BrowserRouter>
@@ -21,6 +22,43 @@ const App = () => {
             </button>
           </TooltipComponent>
         </div>
+        {activeMenu ? (
+      <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+        sidebar
+      </div>  
+      ): (<div className='w-0 dark:bg-secondary-dark-bg'>
+      sidebar w-0
+      </div>
+      )}
+      <div className={
+        `dark:bg-main-bg bg-main-bg min-h-scree 
+        w-full ${activeMenu ? 'md:ml-72' : 'flex'}`
+         }>
+        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+          navbar
+        </div>
+      </div>
+      <div>
+        <Routes>
+          {/* dahboard */}
+          <Route path='/' element="ecommerce"/>
+          <Route path='/' element="ecommerce"/>
+          {/* pages */}
+          <Route path='/order' element="eorder"/>
+          <Route path='/order' element="eorder"/>
+          <Route path='/order' element="eorder"/>
+          {/* Apps */}
+          <Route path='/kanban' element="Kanban"/>
+          <Route path='/editor' element="editor"/>
+          <Route path='/calender' element="calender"/>
+          <Route path='/colorpicker' element="colorpicker"/>
+
+          {/* charts */}
+          <Route path='/colorpicker' element="colorpicker"/>
+          <Route path='/colorpicker' element="colorpicker"/>
+          
+        </Routes>
+      </div>
       </div>
       </BrowserRouter>
     </div>
