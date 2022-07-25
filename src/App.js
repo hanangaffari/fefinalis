2034318251429d12e54f29245330c927b028c1ae
hanangaffari <gaffarihanan@yahoo.com>
2022-07-25 20:16:39 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React,{useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import {FiSettings} from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
